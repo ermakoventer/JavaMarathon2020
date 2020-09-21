0fefc7eb6f90bd308442e3d0380c2b08b3d75e32
Gennadiy Ulyanov <ermakoventer777@gmail.com>
2020-09-21 15:19:29 +0300

    Add task1-6

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int t = 0;
+        while(t < 10) {
+            System.out.print("JAVA ");
+            t++;
+        }
     }
 }
